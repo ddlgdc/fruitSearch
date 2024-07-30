8aa1a62230b2e227931fcdae2521382be7d33f9a
ddlgdc <166570618+ddlgdc@users.noreply.github.com>
2024-07-29 17:52:08 -0700

    search function returns filtered results

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,19 +1,25 @@
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
-
+const typedWord = [];
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-function search(str) {
-	let results = [];
 
-	// TODO
+function search() {
+	const query = typedWord.join('');
+	const filteredRes = fruit.filter(fruit =>
+		fruit.toLowerCase().includes(query.toLowerCase())
+	);
 
-	return results;
+	console.log('Filtered results:', filteredRes);
 }
 
+/*
+function explanation: 
+function collects user input and adds it to an array
+if backspace is pressed, the previous input is removed 
+it also logs the input and the array in string form
+*/
 
-
-const typedWord = [];
 function searchHandler(e) {
 	if (e.key === 'Backspace'){
 		typedWord.pop();	
@@ -24,9 +30,10 @@ function searchHandler(e) {
 	}
 
 
-	console.log('You typed:', e.key);
+	// console.log('You typed:', e.key);
+	// console.log(typedWord.join(''));
 	console.log(typedWord.join(''));
-	console.log('Array contains:', typedWord)
+	search();
 }
 
 
