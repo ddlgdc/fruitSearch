a24aafe0cc14b017415ee9fedd9d58f0f6eb5e1b
ddlgdc <166570618+ddlgdc@users.noreply.github.com>
2024-07-29 18:38:16 -0700

    suggestions are now displayed with the filtered results

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,16 +1,18 @@
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
-const typedWord = [];
+let typedWord = [];
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
-
+let filteredRes = [];
 
 function search() {
 	const query = typedWord.join('');
-	const filteredRes = fruit.filter(fruit =>
+	filteredRes = query ? fruit.filter(fruit =>
 		fruit.toLowerCase().includes(query.toLowerCase())
-	);
+	) : [];
+
 
 	console.log('Filtered results:', filteredRes);
+	showSuggestions();
 }
 
 /*
@@ -38,14 +40,25 @@ function searchHandler(e) {
 
 
 
-function showSuggestions(results, inputVal) {
+function showSuggestions() {
+	suggestions.innerHTML = ''; // clears previous suggs.
 
-	// TODO
-}
+	if (typedWord.length === 0) {
+		return; // do not show suggs. if input is empty 
+	}
 
-function useSuggestion(e) {
-	// TODO
+	filteredRes.forEach(word => {
+		const li = document.createElement('li');
+		li.textContent = word;
+		li.addEventListener('click', () => {
+			console.log('Selected:', word);
+			typedWord = [];
+			input.value = word;
+			showSuggestions();
+		})
+		suggestions.appendChild(li);
+	});
+
 }
 
 input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
